3d404a11f447347b44d1976b046f67681e27ca60
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-08-12 10:41:59 +0300

    added boostrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
      <p> Created by Daria Spytska and is {" "}
      <a href="https://github.com/dariaspytska/weather-app-react" target="_blank">open-sourced on GitHub</a>
      </p>
+     </div>
     </div>
   );
 }
