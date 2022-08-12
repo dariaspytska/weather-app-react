616c71abce98734a3cd3a48ebb55f9f6e70ae5ed
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-08-12 12:30:49 +0300

    add some html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1>Weather App</h1>
+     <Weather />
      <p> Created by Daria Spytska and is {" "}
      <a href="https://github.com/dariaspytska/weather-app-react" target="_blank">open-sourced on GitHub</a>
      </p>
---
+++ src/Weather.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+   return (
+     <div className="Weather">
+     <div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Enter a city..." aria-label="City" aria-describedby="basic-addon2" />
+  <input type="submit" value="Search" className="btn btn-primary" />
+</div>
+       <div className="row">
+         <div className="col-2">
+           <img
+             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+             alt="Mostly Cloudy"
+           ></img>
+           6°C
+         </div>
+         <div className="col-4">
+           <ul>
+             <li>Precipitation: 1%</li>
+             <li>Humidity: 51%</li>
+             <li>Wind: 8 km/h</li>
+           </ul>
+         </div>
+
+         <div className="col-6">
+           <h1>New York</h1>
+           <ul>
+             <li>Friday: 4:00</li>
+             <li>Mostly cloudy</li>
+           </ul>
+         </div>
+       </div>
+      </div>
+   
+   );
+}
