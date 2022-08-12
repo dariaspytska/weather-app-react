040bf794caa127424e81352ace0fa4fa75d0d82a
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-08-12 10:14:06 +0300

    add space between link

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
-     <p> Created by Daria Spytska and is
-     <a href="https://github.com/dariaspytska/weather-app-react" target="_blank"> open-sourced on GitHub</a>
+     <p> Created by Daria Spytska and is {" "}
+     <a href="https://github.com/dariaspytska/weather-app-react" target="_blank">open-sourced on GitHub</a>
      </p>
     </div>
   );
