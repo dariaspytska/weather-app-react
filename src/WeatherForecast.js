056948fa075de580bcaaddefe04abea6f4e4d65f
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-08-21 20:46:17 +0300

    fix

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import WeatherIcon from "./WeatherIcon";
+import React from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
