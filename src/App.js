ffa41ae021a6986c5986b6ebe9edeffd8efb45b6
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-08-21 22:00:27 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <Weather defaultCity="Tokyo"/>
-     <p> Created by Daria Spytska and is {" "}
-     <a href="https://github.com/dariaspytska/weather-app-react" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
-     </p>
-     </div>
+        <Weather defaultCity="Tokyo" />
+        <p> Open-sourced {" "}
+        <a
+          href="https://github.com/dariaspytska/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on GitHub
+        </a>
+        </p>
+      </div>
     </div>
   );
 }
